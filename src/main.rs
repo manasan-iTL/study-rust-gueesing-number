ce00bc4319c2b464f49b2dbdec3deeba87bf64ad
manasan-iTL <daiyaofssh2020@gmail.com>
2023-02-09 19:49:07 +0900

    数当てゲーム　最初の10行目まで理解

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io; // std＝標準ライブラリ > io(入出力のライブラリ)
+
+// main()→エントリポイント
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess");
+
+    // 変数はデフォルトではImmutable
+    // = →　束縛（代入ではない）　guessはStringのnew()で生成される値に束縛される
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    
+    println!("You guessed: {}", guess)
 }
